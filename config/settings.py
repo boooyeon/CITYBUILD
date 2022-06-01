f8df84d7adac8f45e9f4861cf5d6e28d864ffa9e
김보연 (신입사원) <10153351@kt.com>
2022-06-01 16:49:15 +0900

    footer추가

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['config/templates'],
+        'DIRS': ['config/templates/config'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
