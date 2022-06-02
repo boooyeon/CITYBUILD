8d0f0484238df367f255a507d146e32e880659f9
monat96 <sptmgh96@gmail.com>
2022-06-02 23:48:34 +0900

    [완료] 회원가입&login&logout

--- Accountsapp/urls.py
+++ Accountsapp/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
-from . import views
+from django.urls import path
+
+from Accountsapp import views
 
 urlpatterns = [
-     path('signup/', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_accounts, name='login'),
+    path('logout/', views.logout_accounts, name='logout'),
 ]
