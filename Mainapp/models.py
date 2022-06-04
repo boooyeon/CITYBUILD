43becb1369f972dc43c43212339244bef5a86556
monat96 <sptmgh96@gmail.com>
2022-06-05 00:09:21 +0900

    [수정] models 변경

--- Mainapp/models.py
+++ Mainapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Lane(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
@@ -9,22 +10,22 @@ class Lane(models.Model):
     class Meta:
         db_table = 'Lane'
 
-class Building(Lane, models.Model):
+
+class Address(models.Model):
     lane_id = models.ForeignKey("Lane", related_name="lane", on_delete=models.CASCADE, db_column="lane_id")
     city = models.CharField(max_length=32)  # 시
     county = models.CharField(max_length=32)  # 군
     district = models.CharField(max_length=32)  # 구
     road_address = models.CharField(max_length=64)  # 도로명주소
-    
+
     class Meta:
-        db_table = 'Building'
+        db_table = 'Address'
+
 
 class Scrap(models.Model):
-    scrap_id = models.AutoField(primary_key=True)
     lane_id = models.ForeignKey('Lane', on_delete=models.CASCADE, db_column='lane_id')
     user_id = models.ForeignKey('User', on_delete=models.CASCADE, db_column='user_id')
 
     class Meta:
         db_table = 'Scrap'
         managed = False
-
